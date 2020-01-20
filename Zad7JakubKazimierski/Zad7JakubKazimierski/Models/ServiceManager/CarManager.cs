f6b5607b2bc6e80dda105b3ededef726e5e6ac29
Jakub <j.m.kazimierski@gmail.com>
2020-01-20 15:01:06 +0100

    class CarManager manages all DB operations on Car table it's service for Car Table

---
+++ Zad7JakubKazimierski/Zad7JakubKazimierski/Models/ServiceManager/CarManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Zad7JakubKazimierski.Models.Services;
+
+namespace Zad7JakubKazimierski.Models.ServiceManager
+{
+    public class CarManager : IDataService<Car>
+    {
+        readonly VehicleContext _carContext;
+
+        public CarManager(VehicleContext context)
+        {
+            _carContext = context;
+        }
+
+        public IEnumerable<Car> GetAll()
+        {
+            return _carContext.Cars.ToList();
+        }
+
+        public Car Get(int id)
+        {
+            return _carContext.Cars.FirstOrDefault(e => e.CarId == id);
+        }
+
+        public void Add(Car entity)
+        {
+            _carContext.Cars.Add(entity);
+            _carContext.SaveChanges();
+        }
+
+        public void Update(Car car, Car entity)
+        {
+            car.CarModel = entity.CarModel;
+            car.CarProducer = entity.CarProducer;
+            car.DateOfProduction = entity.DateOfProduction;
+            car.CarPrice = entity.CarPrice;
+
+            _carContext.SaveChanges();
+
+        }
+
+        public void Delete(Car car)
+        {
+            _carContext.Cars.Remove(car);
+            _carContext.SaveChanges();
+        }
+    }
+}
